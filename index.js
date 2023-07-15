8ec7ba8e7ae3580283cf0eb4d4c681e5edf4ce33
bforbit <115847484+bforbit@users.noreply.github.com>
2023-07-15 13:09:11 -0400

    Commit initial files

---
+++ index.js
@@ -0,0 +1,37 @@
+// HOME CODE
+let homeEl = document.getElementById("home-el")
+let homeCount = 0
+
+function home1Add() {
+    homeCount += 1
+    homeEl.textContent = homeCount
+}
+
+function home2Add() {
+    homeCount += 2
+    homeEl.textContent = homeCount
+}
+
+function home3Add() {
+    homeCount += 3
+    homeEl.textContent = homeCount
+}
+
+// GUEST CODE
+let guestEl = document.getElementById("guest-el")
+let guestCount = 0
+
+function guest1Add() {
+    guestCount += 1
+    guestEl.textContent = guestCount
+}
+
+function guest2Add() {
+    guestCount += 2
+    guestEl.textContent = guestCount
+}
+
+function guest3Add() {
+    guestCount += 3
+    guestEl.textContent = guestCount
+}
